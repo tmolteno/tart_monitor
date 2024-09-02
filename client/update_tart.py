92970496b0617684af6191378ea75ff0d21b57e7
Tim Molteno <tim@elec.ac.nz>
2024-09-02 16:58:53 +1200

    Add a basic client

--- client/update_tart.py
+++ client/update_tart.py
@@ -1,8 +1,20 @@
-
+import os
 import requests
+import time
 
 # Make a GET request to a specified URL
-response = requests.get('http://api.elec.ac.nz/tart_monitor')
 
-# Print the response text
-print(response.text)
+tart_name=os.environ['TART_API']
+
+while True:
+    try:
+        print(f"Starting ping")
+        response = requests.get('http://api.elec.ac.nz/tart_monitor')
+
+        # Print the response text
+        print(response.text)
+    except Exception as e:
+        print(e)
+    finally:
+        print("sleeping")
+        time.sleep(60)
